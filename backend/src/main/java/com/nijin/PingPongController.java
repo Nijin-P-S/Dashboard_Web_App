092e2268f04ee6be4dd937c722462b6fa61baaef
Nijin P S <nijingithub@gmail.com>
2023-08-09 11:29:34 +0530

    CD workflow with new controller

--- backend/src/main/java/com/nijin/PingPongController.java
+++ backend/src/main/java/com/nijin/PingPongController.java
@@ -1,4 +1,14 @@
 package com.nijin;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PingPongController {
+    record PingPong(String result){}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong(){
+        return new PingPong("Pong")
+    }
 }
